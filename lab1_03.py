9d40cdf60fb0a082559ead5250cb9e42677a95ac
sitthatot <51449357+sitthatot@users.noreply.github.com>
2022-08-17 10:35:11 +0700

    Delete lab1_03.py

--- lab1_03.py
+++
@@ -1,9 +0,0 @@
-print(" *** Summation of each digit ***")
-number = input("Enter a positive number : ")
-saperator = [int(d) for d in str(number)]
-collect = 0
-for item in saperator:
-    collect += item
-print(f"Summation of each digit =  {collect}")
-
-
